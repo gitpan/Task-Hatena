e5d2d4dd95ff20759266a1c4e3ec529c1eb61189
Kevin William Henwood <ski@cpan.org>
2007-07-21 00:42:41 +0000

    Import of SKI/Task-Hatena-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Hatena
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SKI/Task-Hatena-0.01.tar.gz
    gitpan-cpan-author:       SKI
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Hatena.pm
@@ -0,0 +1,246 @@
+package Task::Hatena;
+
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+
+=head1 NAME
+
+Task::Hatena - installs all the Hatena modules
+
+=head1 VERSION
+
+Task::Hatena 0.01
+
+=head1 SYNOPSIS
+
+  cpan Task::Hatena
+
+=head1 DESCRIPTION
+
+This module gathers together all the Hatena modules in one convenient place
+in order to satisfy the requirements of other modules that use Hatena such 
+as Apache, Kwiki, Catalyst, etc.
+
+=head1 INTERFACE
+
+none
+
+=head2 EXPORT
+
+None by default.
+
+=head1 SEE ALSO
+
+See the web site: http://www.hatena.ne.jp/
+
+=head1 DEPENDENCIES
+
+Test::More
+
+Class::Struct::FIELDS
+
+Class::MethodMaker
+
+Class::Generate
+
+Class::Class
+
+Class::Contract
+
+File::Spec
+
+Getopt::Long
+
+Class::ISA
+
+Pragmatic
+
+Class::Trigger
+
+File::Temp
+
+Ima::DBI
+
+List::Util
+
+Clone
+
+UNIVERSAL::moniker
+
+version
+
+Class::Accessor::Fast
+
+Class::ErrorHandler
+
+LWP
+
+Digest::MD5
+
+JSON
+
+Hatena::API::Auth
+
+Text::Balanced
+
+ExtUtils::MakeMaker
+
+Test::File::Contents
+
+Test::Pod
+
+Test::Pod::Coverage
+
+Test::Warn
+
+Spiffy
+
+Filter::Util::Call
+
+Class::Spiffy
+
+Spoon
+
+IO::All
+
+Template
+
+Time::HiRes
+
+LWP
+
+URI
+
+DB_File
+
+Parse::RecDescent
+
+Regexp::Assemble
+
+Test::Base
+
+Text::Hatena
+
+Text::Template
+
+Template::Parser
+
+Date::Parse
+
+Heap::Fibonacci
+
+IO
+
+Cache::File
+
+URI
+
+RPC::XML
+
+MIME::Base64
+
+LWP::Authen::Wsse
+
+Template::Plugin::Hatena
+
+Text::Iconv
+
+Encode
+
+Jcode
+
+Encode::compat
+
+Class::Data::Inheritable
+
+Hatena::keyword
+
+Hatena::Keyword::Similar
+
+Hatena::Formatter
+
+Kwiki
+
+Kwiki::Formatter::Hatena
+
+Kwiki::Theme::Hatena
+
+Math::Trig
+
+Location::GeoTool
+
+WWW::Hatena::WanWanWorld
+
+URI::Fetch
+
+IPC::ShareLite
+
+Cache::FileCache
+
+URI::Fetch::SimpleCache
+
+XML::Parser
+
+XML::SAX
+
+XML::NamespaceSupport
+
+XML::Simple
+
+WebService::Hatena::Exist
+
+Errno
+
+HTTP::Lite
+
+XML::TreePP
+
+WebService::Hatena::BookmarkCount
+
+WebService::Hatena::AsinCount
+
+JSON::Any
+
+WebService::Hatena::Graph
+
+XML::LibXML
+
+XML::LibXML::Common
+
+XML::DOM
+
+XML::RSS
+
+XML::XPath
+
+XML::Atom
+
+Image::Info
+
+WebService::Hatena::Fotolife
+
+WebService::SyncSBS::D2H
+
+=head1 AUTHOR
+
+Kevin W. Henwood, <Khen1950fx@aol.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2007 by Kevin W. Henwood
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.6.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
+
+1;
